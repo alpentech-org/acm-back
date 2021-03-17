b1297caf18f5856265e93007e80ff1f33b41147a
alpentech <tguilhin@alpen-tech.com>
2021-03-17 16:07:42 +0100

    Ajout du MiddleWare pour gestion CORS

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+// CORS gestion
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use((req, res) => {
    res.json({ message: 'Réponse Serveur !' });
 });
