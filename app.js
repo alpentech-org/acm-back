77a0591bc12543e5ae7d81d6796074daf3a3d0d3
alpentech <tguilhin@alpen-tech.com>
2021-03-17 16:23:13 +0100

    Ajout du middleware de connection à la base de données MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Database connection middleware
+mongoose.connect('mongodb://127.0.0.1:27017',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // CORS gestion
 app.use((req, res, next) => {
