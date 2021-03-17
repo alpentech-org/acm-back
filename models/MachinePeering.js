93c10065a4caf644718c4469af7871617b0098bf
alpentech <tguilhin@alpen-tech.com>
2021-03-17 19:54:16 +0100

    Ajout de la validation sur modèle machinePeering

--- models/MachinePeering.js
+++ models/MachinePeering.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const machinePeeringSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  ellisettingId: { type: String, required: true },
-  coscomId: { type: String },
-  coscomAutoMode: { type: Boolean },
+  name: {
+    type: String,
+    required: [true, "Le nom est obligatoire"],
+    minLength: [3, "Le nom doit faire au moins 3 caractères"],
+    maxLength: [50, "Le nom fait au maximum 50 caractères"],
+    unique: [true, "Le nom doit être unique"],
+  },
+  ellisettingId: {
+    type: String,
+    required: [true, "L'Id ElliSetting est obligatoire"],
+  },
+  coscomId: {
+    type: String,
+  },
+  coscomAutoMode: {
+    type: Boolean,
+  },
 });
+
+machinePeeringSchema.plugin(uniqueValidator, { message: 'La propriété {PATH} doit être unique'});
 
 module.exports = mongoose.model('MachinePeering', machinePeeringSchema);
