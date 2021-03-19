85f88dcfc67cb3909170e37dfaadd48d7274613f
alpentech <tguilhin@alpen-tech.com>
2021-03-19 14:13:23 +0100

    Ajout d'une route de suppression d'un user

--- controllers/user.js
+++ controllers/user.js
@@ -98,3 +98,21 @@ exports.setRights = (req, res, next) => {
       }
     );
 };
+
+exports.deleteUser = (req, res, next) => {
+  User.deleteOne({
+    _id: req.params.id
+  }).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,6 @@ const configAuth = require('../middleware/configAuth');
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.put('/setrights/:id', configAuth, userCtrl.setRights)
+router.delete('/delete/:id', configAuth, userCtrl.deleteUser)
 
 module.exports = router;
