428a6e6b32b3a5f2ee7247548e1c56137a2ca0bd
alpentech <tguilhin@alpen-tech.com>
2021-03-22 08:23:43 +0100

    Changement de la route vers le service d'authentification

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/api/user', userRoutes);
+app.use('/api/auth', userRoutes);
 app.use('/api/machinePeering', auth, machinePeeringRoutes);
 
 app.use((req, res) => {
