e83f19b61fd1852ca08d31ebdf3b5ae702325596
alpentech <tguilhin@alpen-tech.com>
2021-03-23 14:59:27 +0100

    Harmonisation des routes /user

--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,8 @@ const configAuth = require('../middleware/configAuth');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
-router.put('/setrights/:id', configAuth, userCtrl.setRights)
-router.delete('/delete/:id', configAuth, userCtrl.deleteUser)
+router.put('/users/setrights/:id', configAuth, userCtrl.setRights)
+router.delete('users/:id', configAuth, userCtrl.deleteUser)
 router.get('/users', configAuth, userCtrl.getAllUsers)
 
 module.exports = router;
