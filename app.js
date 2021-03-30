4c118ecc849de8c52375d1ca2eb7239371e060f0
alpentech <tguilhin@alpen-tech.com>
2021-03-30 18:42:16 +0200

    Gestion du Pre-Flight avant le middleWare d'authentification

--- app.js
+++ app.js
@@ -27,7 +27,11 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(204);
+  } else {
+    next();
+  }
 });
 
 app.use(bodyParser.json());
