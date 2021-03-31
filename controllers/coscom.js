94c75fa7ab221175fffc8046d253a81407a9d3a8
alpentech <tguilhin@alpen-tech.com>
2021-03-31 16:21:04 +0200

    Ajout de l'endPoint getCoscomMachineById

--- controllers/coscom.js
+++ controllers/coscom.js
@@ -63,3 +63,57 @@ exports.getMachines = (req, res, next) => {
   connection.connect();
 
 };
+
+exports.getMachineById = (req, res, next) => {
+
+  let id = req.params.id;
+
+  var connection = new Connection(sqlConfig);
+  // connection.on('debug', msg => {
+  //   console.log('CONNECTION DEBUG : ' + msg);
+  // });
+
+  connection.on('connect', function(err) {
+    let coscomRequest = new Request('\
+      SELECT TRS_RES.RES_ID, TRS_RES.DISPLAYNAME_FR\
+      FROM TRS_RES\
+      WHERE (TRS_RES.RES_TYPE_ID=\'C_MACH\') AND (TRS_RES.RES_ID=\'' + id + '\')\
+      ORDER BY TRS_RES.DISPLAYNAME_FR;\
+        ', function(err) {
+      if (err) {
+        res.json(err);
+      }
+    });
+    var machineList = [];
+    var machine = {};
+    coscomRequest.on('row', function(columns) {
+      let i = 0;
+      columns.forEach(function(column) {
+        if (i === 0) {
+          machine.id = column.value;
+        } else if (i === 1) {
+          machine.name = column.value;
+        }
+        i++;
+      });
+      machineList.push(machine);
+      machine = {};
+    });
+
+    coscomRequest.on('requestCompleted', function() {
+      connection.close();
+      if (machineList.length) {
+        res.status(200).json(machineList[0]);
+      } else {
+        res.status(204).send();
+      }
+
+    });
+
+    connection.execSql(coscomRequest);
+
+  });
+
+  connection.connect();
+
+};
--- routes/coscom.js
+++ routes/coscom.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const coscomCtrl = require('../controllers/coscom');
 
 router.get('/machines', coscomCtrl.getMachines);
+router.get('/machines/:id', coscomCtrl.getMachineById);
 
 module.exports = router;
