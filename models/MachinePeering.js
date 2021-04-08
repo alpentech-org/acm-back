a30f9180b3747dbd0a5ed35fc4bec47fc55cbc92
alpentech <74764431+alpentech@users.noreply.github.com>
2021-04-08 10:20:15 +0200

    Correction du modèle machinePeering

--- models/MachinePeering.js
+++ models/MachinePeering.js
@@ -4,6 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator');
 const machinePeeringSchema = mongoose.Schema({
   name: {
     type: String,
+    trim: true,
     required: [true, "Le nom est obligatoire"],
     minLength: [3, "Le nom doit faire au moins 3 caractères"],
     maxLength: [50, "Le nom fait au maximum 50 caractères"],
@@ -16,13 +17,26 @@ const machinePeeringSchema = mongoose.Schema({
   },
   coscomId: {
     type: String,
-    unique: true,
   },
   coscomAutoMode: {
     type: Boolean,
+    required: [true, "Le mode auto coscom est obligatoire"],
   },
 });
 
-machinePeeringSchema.plugin(uniqueValidator, { message: 'La propriété {PATH} doit être unique'});
+machinePeeringSchema.index({
+  coscomId: 1
+}, {
+  unique: true,
+  partialFilterExpression: {
+    'coscomId': {
+      $type: "string"
+    }
+  }
+});
+
+machinePeeringSchema.plugin(uniqueValidator, {
+  message: 'La propriété {PATH} doit être unique'
+});
 
 module.exports = mongoose.model('MachinePeering', machinePeeringSchema);
