9765673ebe7d5875bc1c2aa318647b0ddfd2bf04
alpentech <74764431+alpentech@users.noreply.github.com>
2021-04-15 16:21:59 +0200

    Ajout de la route getStatusList pour le service coscom

--- controllers/coscom.js
+++ controllers/coscom.js
@@ -1,4 +1,5 @@
 const config = require('../config/config');
+const moment = require('moment');
 const Connection = require('tedious').Connection;
 const Request = require('tedious').Request;
 const TYPES = require('tedious').TYPES;
@@ -104,6 +105,77 @@ exports.getMachineById = (req, res, next) => {
       connection.close();
       if (machineList.length) {
         res.status(200).json(machineList[0]);
+      } else {
+        res.status(204).send();
+      }
+
+    });
+
+    connection.execSql(coscomRequest);
+
+  });
+
+  connection.connect();
+
+};
+
+exports.getStatusList = (req, res, next) => {
+
+  if (!req.query || !req.query.start || !req.query.end || !req.query.machineId) {
+    res.status(400).send({
+      error: "Paramètre de requête manquant"
+    });
+  }
+
+  let formattedStart = moment(req.query.start).format('YYYY-MM-DD HH:mm:ss.SSS');
+  let formattedEnd = moment(req.query.end).format('YYYY-MM-DD HH:mm:ss.SSS');
+
+  var connection = new Connection(sqlConfig);
+  // connection.on('debug', msg => {
+  //   console.log('CONNECTION DEBUG : ' + msg);
+  // });
+
+  connection.on('connect', function(err) {
+    let coscomRequest = new Request('\
+    SELECT TSF_RES_INTERVAL.RES_ID, TRS_RES.DISPLAYNAME_FR, TSF_RES_INTERVAL.BEGIN_TS, TSF_RES_INTERVAL.END_TS, TRS_RES_STATUS.RES_STATUS_ID, TRS_RES_STATUS.DISPLAYNAME_FR, TSF_RES_INTERVAL.KOMMENTAR\
+    FROM (TSF_RES_INTERVAL INNER JOIN TRS_RES ON TSF_RES_INTERVAL.RES_ID = TRS_RES.RES_ID) INNER JOIN TRS_RES_STATUS ON TSF_RES_INTERVAL.RES_STATUS_ID = TRS_RES_STATUS.RES_STATUS_ID\
+    WHERE (((TRS_RES.RES_ID)=\'' + req.query.machineId + '\') AND ((TSF_RES_INTERVAL.END_TS)>=\'' + formattedStart + '\') AND ((TSF_RES_INTERVAL.BEGIN_TS)<=\'' + formattedEnd + '\'))\
+    ORDER BY TRS_RES.DISPLAYNAME_FR, TSF_RES_INTERVAL.CAL_DAY, TSF_RES_INTERVAL.BEGIN_TS;\
+        ', function(err) {
+      if (err) {
+        res.json(err);
+      }
+    });
+    var statusList = [];
+    var status = {};
+    coscomRequest.on('row', function(columns) {
+      let i = 0;
+      columns.forEach(function(column) {
+        if (i === 0) {
+          status.machineId = column.value;
+        } else if (i === 1) {
+          status.machineName = column.value;
+        } else if (i === 2) {
+          status.start = column.value;
+        } else if (i === 3) {
+          status.end = column.value;
+        } else if (i === 4) {
+          status.statusId = column.value;
+        } else if (i === 5) {
+          status.statusName = column.value;
+        } else if (i === 6) {
+          status.comment = column.value;
+        }
+        i++;
+      });
+      statusList.push(status);
+      status = {};
+    });
+
+    coscomRequest.on('requestCompleted', function() {
+      connection.close();
+      if (statusList.length) {
+        res.status(200).json(statusList);
       } else {
         res.status(204).send();
       }
--- routes/coscom.js
+++ routes/coscom.js
@@ -5,5 +5,6 @@ const coscomCtrl = require('../controllers/coscom');
 
 router.get('/machines', coscomCtrl.getMachines);
 router.get('/machines/:id', coscomCtrl.getMachineById);
+router.get('/statuslist', coscomCtrl.getStatusList);
 
 module.exports = router;
