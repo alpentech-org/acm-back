c6d8584722ce65f6b78889fa41727867fa9cfe60
slievreATI <slievre@alpen-tech.com>
2023-04-27 08:51:54 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,18 @@
+
+/**
+ * la variable config est un objet qui contient les variables d'environnement
+ * @typedef {{}} Config 
+ * @property {number} port - Le port sur lequel le serveur doit écouter
+ * @property {string} dbName - Le nom de la base de données
+ * @property {string} dbPort - Le port sur lequel la base de données écoute
+ * @property {string} dbUrl - L'adresse de la base de données
+ * @property {string} ellisettingUrl - L'adresse de l'API ellisetting
+ * @property {string} ellisettingApiKey - La clé d'API de l'API ellisetting
+ * @property {string} ellisettingUsername - Le nom d'utilisateur de l'API ellisetting
+ * 
+ */
 let config = {
-    port: process.env.ENV === 'PROD' ? process.env.PROD_PORT : (process.env.ENV === 'DEV' ? process.env.DEV_PORT : false),
+    port: process.env.ENV === 'PROD' ? 3000 : (process.env.ENV === 'DEV' ? 3099 : false),
     dbName: process.env.ENV === 'PROD' ? process.env.PROD_DB : (process.env.ENV === 'DEV' ? process.env.DEV_DB : false),
     dbPort: process.env.DB_PORT,
     dbUrl: process.env.DB_URL,
@@ -8,4 +21,8 @@ let config = {
     ellisettingUsername: process.env.ELLISETTING_USERNAME,
 }
 
+// module.exports permet de rendre l'objet config accessible depuis d'autres fichiers en utilisant require()
+/**
+ * @return {Config}
+ */
 module.exports = config;
